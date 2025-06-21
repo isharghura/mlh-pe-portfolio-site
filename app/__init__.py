7cc9fce89bf064f218e68c3bffd323ed7c8ccb64
isharghura <ishar.ghuras@gmail.com>
2025-06-21 17:58:33 -0400

    feat: add work experience + use Jinja to make template for multiple

--- app/__init__.py
+++ app/__init__.py
@@ -15,16 +15,42 @@ nav_items = [
     {"name": "Travel", "url": "/travel"},
 ]
 
+
 # make nav items global
 @app.context_processor
 def inject_nav_items():
     return dict(nav_items=nav_items)
+
 
 # routes
 @app.route("/")
 def index():
     return render_template("index.html", title="Home")
 
+
 @app.route("/about")
 def about():
     return render_template("about.html", title="About")
+
+
+@app.route("/work")
+def work():
+    work_experiences = [
+        {
+            "company": "JSI Telecom",
+            "role": "Software Developer",
+            "date": "Sep 2025 - Dec 2025",
+        },
+        {"company": "MLH Fellowship", "role": "Fellow", "date": "Jun 2025 - Sep 2025"},
+        {
+            "company": "SunnySide",
+            "role": "Software Engineer",
+            "date": "Apr 2024 - Present",
+        },
+        {
+            "company": "Global Affairs Canada",
+            "role": "Junior Developer",
+            "date": "Jun 2024 - Dec 2024",
+        },
+    ]
+    return render_template("work.html", jobs=work_experiences)
