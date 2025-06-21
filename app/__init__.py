946c7cc5b5bb9bdfbf0e587ccb8cb51f4b65a2f1
isharghura <ishar.ghuras@gmail.com>
2025-06-21 13:15:04 -0400

    feat: add nav bar

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,30 @@
-import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 app = Flask(__name__)
 
+# define nav items
+nav_items = [
+    {"name": "Home", "url": "/"},
+    {"name": "About", "url": "/about"},
+    {"name": "Work", "url": "/work"},
+    {"name": "Hobbies", "url": "/hobbies"},
+    {"name": "Education", "url": "/education"},
+    {"name": "Travel", "url": "/travel"},
+]
 
-@app.route('/')
+# make nav items global
+@app.context_processor
+def inject_nav_items():
+    return dict(nav_items=nav_items)
+
+# routes
+@app.route("/")
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    return render_template("index.html", title="Home")
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title="About")
