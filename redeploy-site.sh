2626c135f260b1c3294036abdf104f19dfc961dc
Ishar Ghura <ishar.ghuras@gmail.com>
2025-07-05 23:04:55 -0400

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -21,6 +21,6 @@ cd /root/mlh-pe-portfolio-site
 source python3-virtualenv/bin/activate
 export FLASK_APP=app.py
 export FLASK_ENV=production
-flask run --host=0.0.0.0 --port=5000
+exec flask run --host=0.0.0.0 --port=5000
 
 echo "deployment complete, flask server is running"
