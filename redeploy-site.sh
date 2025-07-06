00c0d4b8b84739d8c1e6a9ce64a01c95235d2d23
Ishar Ghura <ishar.ghuras@gmail.com>
2025-07-05 23:10:45 -0400

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+PROJECT_DIR="/root/mlh-pe-portfolio-site"
+
 echo "Killing all tmux sessions"
 tmux kill-server
 
 echo "Changing directory to proj folder"
-cd mlh-pe-portfolio-site
+cd "${PROJECT_DIR}"
 
 echo "fetching latest changes from github"
 git fetch && git reset origin/main --hard
@@ -16,11 +18,12 @@ echo "installing requirements"
 pip install -r requirements.txt
 
 echo "starting tmux session"
-tmux new-session -d -s flask-site
-cd /root/mlh-pe-portfolio-site
-source python3-virtualenv/bin/activate
-export FLASK_APP=app.py
-export FLASK_ENV=production
-exec flask run --host=0.0.0.0 --port=5000
+TMUX_COMMAND="cd \"${PROJECT_DIR}\" && \
+              source python3-virtualenv/bin/activate && \
+              export FLASK_APP=app.py && \
+              export FLASK_ENV=production && \
+              exec flask run --host=0.0.0.0 --port=5000"
+              
+tmux new-session -d -s flask-site "${TMUX_COMMAND}"
 
 echo "deployment complete, flask server is running"
