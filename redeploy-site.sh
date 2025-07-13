27e892202338248f55d582299448ae2875bd82d1
isharghura <ishar.ghuras@gmail.com>
2025-07-13 16:55:23 -0400

    feat: repdeploy now uses services instead of tmux

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -3,30 +3,23 @@ set -e
 
 PROJECT_DIR="/root/mlh-pe-portfolio-site"
 
-echo "killing all tmux sessions"
-tmux kill-server || true
-
-echo "changing directory to project folder"
+echo "Changing directory to project folder"
 cd "${PROJECT_DIR}"
 
-echo "fetching latest changes from GitHub"
+echo "Fetching latest changes from GitHub"
 git fetch && git reset origin/main --hard
 
-echo "activating virtual environment"
+echo "Activating virtual environment"
 source python3-virtualenv/bin/activate
 
-echo "installing requirements"
+echo "Installing requirements"
 pip install -r requirements.txt
 
-echo "start tmux session"
+echo "Restarting myportfolio service"
+sudo systemctl restart myportfolio
 
-COMMAND_TO_RUN="
-cd '${PROJECT_DIR}' &&
-source python3-virtualenv/bin/activate &&
-export FLASK_APP=app.py &&
-export FLASK_ENV=production &&
-exec flask run --host=0.0.0.0 --port=5000
-"
-tmux new-session -d -s flask-site "bash -c \"${COMMAND_TO_RUN}\""
+echo "Checking service status"
+sudo systemctl status myportfolio --no-pager
 
-echo "deployment complete"
+echo "Deployment complete"
+echo "Your portfolio site should now be running with the latest changes"
