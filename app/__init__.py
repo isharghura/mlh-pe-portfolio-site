d4c0af5a795f9cc1ed0ff1aca4747128403b4eca
Ishar Ghura <ishar.ghuras@gmail.com>
2025-07-13 12:52:48 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from dotenv import load_dotenv
 from peewee import *
 import os
+import datetime
 
 load_dotenv()
 app = Flask(__name__)
@@ -19,6 +20,18 @@ mydb = MySQLDatabase(
     port=3306
 )
 print(mydb)
+
+class TimelinePost(Model):
+    name = CharField()
+    email = CharField()
+    content = TextField()
+    created_at = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = mydb
+
+mydb.connect()
+mydb.create_tables([TimelinePost])
 
 # define nav items
 nav_items = [
@@ -113,3 +126,22 @@ def travel():
         {"name": "Hawaii, USA", "lat": 19.8968, "lng": -155.5828}
     ]
     return render_template("travel.html", locations=locations)
+
+@app.route('/api/timeline_post', methods=['POST'])
+def post_time_line_post():
+    name = request.form['name']
+    email = request.form['email']
+    content = request.form['content']
+    timeline_post = TimelinePost.create(name=name, email=email, content=content)
+
+    return model_to_dict(timeline_post)
+
+@app.route('/api/timeline_post', methods=['GET'])
+def get_time_line_post():
+    return {
+        'timeline_posts': [
+            model_to_dict(p)
+            for p in
+TimelinePost.select().order_by(TimelinePost.created_at.desc())
+        ]
+    }
